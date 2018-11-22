b81c4b57cfc3edb57d404c35a25e97fc3e6f176b
ElPadrino045 <erikalberto45@gmail.com>
2018-11-22 16:05:11 -0600

    Consultas.

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-
+/*
 [remote "origin"]
-url = https://github.com/ElPadrino045/prueba
-fetch = +refs/heads/*:refs/remotes/origin/*
+url = https://github.com/ElPadrino045/prueba*/
+//fetch = +refs/heads/*:refs/remotes/origin/*
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'classicmodels'
+});
+
+connection.connect();
+
+connection.query('select * from customers', function (error, results, fields) {
+  if (error) throw error;
+  //console.log(results[0]);
+/*  for (var i = 0; i < results.length; i++) {
+    console.log('Tabla número :'+ (i+1)+ "  ",results[i].Tables_in_classicmodels+"\n");
+  }*/
+  for (var i = 0; i < results.length; i++) {
+     //console.log('Registro número :'+ (i+1)+ "  ",results[i]+"\n");
+     console.log('Nombre :'+ (i+1)+ "  ",results[i].customerName);
+   }
+});
+
+connection.end();
